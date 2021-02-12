746df8d82f73bf8946d97ec683a8f8b35704791c
Denis Gallegos <denis.gallegos03@gmail.com>
2021-02-12 13:29:42 -0600

    primero

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <array>
+#include <cstring>
+#include <string>
+
+
+
+
+int main(int argc, char** argv) {
+	
+	
+	
+	
+	
+	
+	return 0;
+}//Fin del MAIN.
